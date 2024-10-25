45cbc3450e730f541d1f125f273d0a013159ae75
Rijal15 <orepmulyo278@gmail.com>
2024-10-25 09:38:04 +0700

    Update main.dart
    Rijal update

--- main.dart
+++ main.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart'; // paket interaksi
 void main() {
   runApp(
     MyApp(
-
     )
   );
 }
@@ -19,7 +18,6 @@ class MyApp extends StatelessWidget {
   @override // mulai ganti kolom kode yang lama yang sudah ada di original ckass dengan kode yang baru
   Widget build(BuildContext context) {
     // mengatur ui, posisi widget
-
     // change notifier profider untuk mendeteksi interaksi di apliksi
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
@@ -43,6 +41,15 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+  var favorites = <WordPair>[]; // membuat variable favorites yang menyimpan list string
+  void toggleFavorites() {
+    if(favorites.contains(current)) { // bila di array ada string dari value current maka string current di array favorites dihapus
+      favorites.remove(current);
+    } else { // bila di array tidak ada string dari value variable current maka array favorites di tambah value dari string current
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -51,19 +58,39 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>(); /// menggunakan state myappstate
     // di bawah ini adalah kode program untuk menyusun layout
     var pair = appState.current; // menyimpan kata yang sedang aktif
-
+    IconData icon;
+    if(appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
     return Scaffold( // base layout
-      body: Column(
-        children: [
-          Text('hello test:'),
-          BigCards(pair: pair),
-          ElevatedButton( // create button
-            onPressed: () { // bila di tekan tombolnya maka akan exekusi function shadow dengan value print hello world
-              appState.getNext();
-            },
-            child: Text("klik"), // ini adalah teks dari button
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCards(pair: pair),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorites(); // saat tombol di tekan maka akan menjalakan function dari class appState
+                  },
+                  icon: Icon(icon), // menentukan ikon
+                  label: Text("Like"), // menentukan label teks
+                ),
+                ElevatedButton( // create button
+                  onPressed: () { // bila di tekan tombolnya maka akan exekusi function shadow dengan value print hello world
+                    appState.getNext();
+                  },
+                  child: Text("klik"), // ini adalah teks dari button
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -82,12 +109,16 @@ class BigCards extends StatelessWidget {
     final theme = Theme.of(context);
     final style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
-    );
+    ); // membuat variable style untuk teks
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase, // mengubah string pair jadi lowe case
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}" // memberi label pada masing masing kata, supaya teks terbaca dengan benar oleh aplikasi
+        ), // variable style untuk teks dimuat
       ),
     );
   }
